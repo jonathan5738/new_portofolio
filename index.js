e1b43bfaca4d8926dc1e3c019ad920c9ca356c28
jonathan nakahonda <jnakahonda@gmail.com>
2022-10-27 16:50:41 +0200

    added client url to allow request from

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const presentationRoutes = require('./routes/presentationRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
 const projectRoutes = require('./routes/projectRoutes')
 
-app.use(cors())
+var whitelist = [process.env.ALLOWED_CLIENT_URL]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true })) 
 
